68891da3005ce92b921b1cbc2555b5c910b01f69
Shubham Karala <shubham@ramanujan.du.ac.in>
2019-10-11 14:10:12 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,7 +35,7 @@ def turn(board)
   user_input = gets.strip.to_i
   index = input_to_index(user_input)
   if board[index] && valid_move?(board, index)
-
+    board[index] = move(board, index, token)
   else
     turn(board)
   end
